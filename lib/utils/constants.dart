aaf9a7f971cf6e9d47c1d7f1c15f66518ae7060e
MrazTevin <millatevin@gmail.com>
2023-09-01 15:19:09 +0300

    m: declare height constant

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 const tDefaultSize = 30.0;
 const tSplashContainerSize = 30.0;
 const tButtonHeight = 15.0;
+const tFormHeight = 30.0;
+
 
 const Color tPrimaryColor = Colors.amberAccent;
 const Color tSecondaryColor = Color(0xFF272727);
